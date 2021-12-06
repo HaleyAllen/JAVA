c2f64b110f34f95e6971b43a2b7642093843694c
HaleyAllen <haleyallen2074@gmail.com>
2021-12-06 00:23:37 -0800

    inital commit

---
+++ FruitStore/src/main/java/Controller/Controller.java
@@ -0,0 +1,34 @@
+package Controller;
+
+import java.util.ArrayList;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.codingdojo.Item;
+
+
+@Controller
+public class  Controller {
+ 
+ @RequestMapping("/")
+ public String index(Model model) {
+     
+     ArrayList<Item> fruits = new ArrayList<Item>();
+     fruits.add(new Item("Kiwi", 1.50));
+     fruits.add(new Item("Mango", 2.00));
+     fruits.add(new Item("Goji Berries", 4.00));
+     fruits.add(new Item("Guava", .75));
+     fruits.add(new Item("Banana", .25));
+     fruits.add(new Item("Pineapple", 3.50));
+     fruits.add(new Item("Apple", 1.25));
+     fruits.add(new Item("Strawberries", 3.75));
+     fruits.add(new Item("Watermelon", 4.25));
+     
+     // Add fruits your view model here
+     
+     model.addAttribute("fruitsFromController", fruits);
+     
+     return "index.jsp";
+ }
+}
